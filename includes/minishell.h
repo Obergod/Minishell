d7fa1727ca03d807d390ab098a90c549d5febfe4
Ugo Falzone <ufalzone@bess-f3r6s4.clusters.42paris.fr>
2025-03-21 16:51:40 +0100

    Minishell.h clean

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ufalzone <ufalzone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 15:16:18 by ufalzone          #+#    #+#             */
-/*   Updated: 2025/03/21 15:42:58 by ufalzone         ###   ########.fr       */
+/*   Updated: 2025/03/21 16:51:23 by ufalzone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,6 @@
 #define MINISHELL_H
 
 #include "../full_libft/include/libft.h"
-
-enum e_pipe
-{
-	START,
-	MIDLE,
-	END
-};
-
-enum e_state
-{
-    NORMAL,
-    IN_SQUOTE,
-    IN_DQUOTE
-};
 
 typedef struct s_minishell
 {
