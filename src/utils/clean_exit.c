ad10bf036c6484c924dd4ce3f282070f9cc334ad
Ugo Falzone <ufalzone@bess-f4r3s3.clusters.42paris.fr>
2025-04-18 19:31:55 +0200

    push

--- src/utils/clean_exit.c
+++ src/utils/clean_exit.c
@@ -6,12 +6,11 @@
 /*   By: ufalzone <ufalzone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 14:55:43 by ufalzone          #+#    #+#             */
-/*   Updated: 2025/04/18 14:57:40 by ufalzone         ###   ########.fr       */
+/*   Updated: 2025/04/18 19:31:08 by ufalzone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/main.h"
-#include <readline/readline.h>
 
 int clean_exit(t_minishell *minishell)
 {
